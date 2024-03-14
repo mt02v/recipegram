ce2b32d4cf0a16b0bebe7efb0c73b80c6c8a0b5b
mt02v <141305346+mt02v@users.noreply.github.com>
2024-03-14 14:06:04 +0000

    [Finish] show,editコントローラー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,13 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+       redirect_to user_path(current_user), alert: "不正なアクセスです。"
+    end
   end
 end
