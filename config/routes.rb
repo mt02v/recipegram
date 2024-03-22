0b88a1b57480437f25de5576bc95ad93d5c7659f
mt02v <141305346+mt02v@users.noreply.github.com>
2024-03-22 14:47:18 +0000

    [Add] routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: "home#index"
 resources :users
+resources :recipes
 end
